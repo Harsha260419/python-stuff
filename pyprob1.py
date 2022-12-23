4b8ca2f501f432814bebdb9472cae9aa53e3ae13
Harsha <harsha.sai2813@gmail.com>
2022-12-23 09:07:08 +0530

    first python problem

---
+++ pyprob1.py
@@ -0,0 +1,12 @@
+l1=[3,6,9,12,15,18,21]
+l2=[4,8,12,16,20,24,28]
+for i in range(0,len(l1)):
+    if i%2!=0:
+        l3=[]
+        l3.append(l1[i])
+for i in range(0,len(l2)):
+    if i%2==0:
+        l4=[]
+        l4.append(l2[i])
+l3.extend(l4)
+print(l3)
